4b080458e1e4533431c71bd48895b4009cb61c10
kate ramshaw <kate.ramshaw@talktalk.net>
2022-07-04 19:06:51 +0100

    added avatar and background styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src="/kateIcon.png"
+        style={{ width: 200, height: 200, borderRadius: 100 }}
+        alt="avatar"
+      />
+      <h1>Hello, you</h1>
     </div>
   );
 }
