b88fc1e3cd8e18fb92fa7cb8e61874ae5b335200
kate.ramshaw <kate.ramshaw@talktalk.net>
2022-08-26 17:00:54 +0100

    added navbutton component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,28 +1,41 @@
-import { AppBar, Container,Toolbar,Box,IconButton,Tooltip,Avatar,Button } from "@mui/material";
-import {Menu,Brightness4,Brightness5} from "@mui/icons-material"
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Box,
+  IconButton,
+  Tooltip,
+  Avatar,
+} from "@mui/material";
+import Brightness4 from "@mui/icons-material/Brightness4";
+import Brightness5 from "@mui/icons-material/Brightness5";
+import Menu from "@mui/icons-material/Menu";
 
-import {useTheme} from "../context/theme"
+import { useTheme } from "../context/theme";
+import Button from "./navButton";
 
 export const Navigation = () => {
   let { theme, toggleTheme } = useTheme();
   return (
     <Container component="main" disableGutters={true} maxWidth={false}>
-      <AppBar>
+      <AppBar sx={{ backgroundColor: "transparent" }}>
         <Toolbar>
-          <IconButton sx={{display:{xs:"flex",md:"none"}}}><Menu/></IconButton>
-            <Avatar src="/favicon-32x32.png" alt="myAvatar"/>
-          <Box sx={{flex:1,display:"flex"}}>
-              <Button>Home</Button>
-              <Button>About</Button>
-              <Button>Projects</Button>
-              <Button>Contact</Button>
+          <IconButton sx={{ display: { xs: "flex", md: "none" } }}>
+            <Menu />
+          </IconButton>
+          <Avatar src="/favicon-32x32.png" alt="myAvatar" />
+          <Box sx={{ flex: 1, display: "flex" }}>
+            <Button label="Home" url="/" />
+            <Button label="About" url="about" />
+            <Button label="Projects" url="projects" />
+            <Button label="Contact" url="contact" />
           </Box>
-          <Box sx={{flexGrow:0}}>
-          <Tooltip title="Toggle light/dark theme">
-      <IconButton onClick={toggleTheme}>
-       { theme === "dark" ? <Brightness4 /> : <Brightness5 />}
-      </IconButton>
-    </Tooltip>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Toggle light/dark theme">
+              <IconButton onClick={toggleTheme}>
+                {theme === "dark" ? <Brightness4 /> : <Brightness5 />}
+              </IconButton>
+            </Tooltip>
           </Box>
         </Toolbar>
       </AppBar>
