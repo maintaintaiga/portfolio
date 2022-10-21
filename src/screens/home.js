11fd4b1df0fd7f41651d9a8969910ba57a65c2ec
kate ramshaw <kate.ramshaw@talktalk.net>
2022-10-21 14:24:50 +0100

    updated home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Typography, Grid } from "@mui/material";
+
+const skills = [
+  "React",
+  "React Native",
+  "Node.JS",
+  "Docker",
+  "Ansible",
+  "Git",
+  "Jira",
+  "MongoDB",
+  "JS",
+  "HTML",
+  "CSS",
+];
 
 export const Home = () => {
   return (
-    <Box>
+    <Box sx={{ width: "50%", my: 4 }}>
       <Typography variant="h1">Frontend</Typography>
       <Typography variant="h1">Developer</Typography>
+      <Box sx={{ p: 4 }}>
+        <Grid container spacing={3}>
+          {skills.map((el) => (
+            <Grid item key={el}>
+              <Chip color="primary" label={el} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   );
 };
