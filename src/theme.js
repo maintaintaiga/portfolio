86c56420ef228dc58fd1d93fcd2f078673e46dd5
kate ramshaw <kate.ramshaw@talktalk.net>
2022-10-21 14:10:07 +0100

    updated theme

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,9 @@ const lightTheme = {
   palette: {
     mode: "light",
     background: { default: "#ffefd5" },
+    primary: {
+      main: "#ffa411",
+    },
   },
 };
 
@@ -9,6 +12,9 @@ const darkTheme = {
   palette: {
     mode: "dark",
     background: { default: "#212121" },
+    primary: {
+      main: "#dda446",
+    },
   },
 };
 
