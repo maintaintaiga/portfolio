cc44d4e2cb6d5ab6067bf3f3d5d858c7945fd2c1
kate.ramshaw <kate.ramshaw@talktalk.net>
2022-11-04 15:29:25 +0000

    added mailjet email utility

---
+++ code/server/util/send-mail.js
@@ -0,0 +1,70 @@
+const mailjet = require("node-mailjet");
+
+const config = require("../../util/env-config");
+const getModulePath = require("../modulePath");
+const logger = require("../winston-config");
+
+const logPath = { label: getModulePath(__filename) };
+
+const sendMail = async (mailTo, mailSubject, mailText, mailHtml) => {
+  try {
+    if (config.emailEnabled === true) {
+      //send mail
+      const mailjetClient = mailjet.connect(
+        config.emailServiceUser,
+        config.emailServicePassword,
+        { version: "v3.1", url: "api.mailjet.com", perform_api_call: true }
+      );
+
+      if (config.userEmailAddressOverride.length > 0) {
+        mailTo = config.userEmailAddressOverride;
+      }
+
+      const message = {
+        From: {
+          Email: config.emailFromAddress,
+          Name: `${config.appPrivateTitle} Web Services`,
+        },
+        To: [
+          {
+            Email: mailTo,
+            //Name: "",
+          },
+        ],
+        Subject: mailSubject,
+        TextPart: mailText,
+        HTMLPart: mailHtml,
+      };
+
+      logger.info(`message: ${JSON.stringify(message)}`, logPath);
+
+      const result = await mailjetClient
+        .post("send")
+        .request({ Messages: [message] });
+
+      if (!result || result.err) {
+        logger.error(
+          `Send mail failed. To: ${mailTo}. Subject: ${mailSubject}`,
+          result.err,
+          logPath
+        );
+        return false;
+      } else {
+        logger.info(
+          `Send mail response: ${JSON.stringify(result.body)}`,
+          logPath
+        );
+        return true;
+      }
+    }
+  } catch (err) {
+    logger.error(
+      `Send mail exception. To: ${mailTo}. Subject: ${mailSubject}`,
+      err,
+      logPath
+    );
+    return false;
+  }
+};
+
+module.exports = { sendMail };
