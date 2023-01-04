c2a0899abde3a79875fd3f24ef65545155cb1f16
kate ramshaw <kate.ramshaw@talktalk.net>
2023-01-04 16:40:12 +0000

    updated contact controller in server

--- code/server/controllers/contact.js
+++ code/server/controllers/contact.js
@@ -1,4 +1,5 @@
 const getModulePath = require("../util/modulePath");
+const { sendMail } = require("../util/send-mail");
 const logger = require("../util/winston-config");
 
 const logPath = { label: getModulePath(__filename) };
@@ -18,9 +19,20 @@ const create = async (req, res, next) => {
   ) {
     try {
       //send email to myself with form data...
-      res.status(201).json(res.body);
+      let data = `<div><h2>Name</h2><p>${req.body.name}</p><h2>Email</h2><p>${req.body.email}</p><h2>Message</h2><p>${req.body.message}</p></div>`;
+      let result = await sendMail(
+        "kate.ramshaw@talktalk.net",
+        "Portfolio Message",
+        "here",
+        data
+      );
+      if (result) {
+        res.status(201).end();
+      } else {
+        res.status(400 /* Bad Request */).end();
+      }
     } catch (err) {
-      logger.error(`Project create failed`, err, logPath);
+      logger.error(`Email Send Failed`, err, logPath);
       return next(extractError(err));
     }
   } else {
