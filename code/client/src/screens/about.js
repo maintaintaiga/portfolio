c722fe4393d4cc3e9a7b54edb2218e8ea5e5c8a1
kate ramshaw <kate.ramshaw@talktalk.net>
2023-01-04 16:41:23 +0000

    added download cv function to about screen

--- code/client/src/screens/about.js
+++ code/client/src/screens/about.js
@@ -1,8 +1,54 @@
+import { useOutletContext } from "react-router-dom";
 import { Fab, Stack, Typography } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
+
 import Header from "../components/header";
+import { ApiAxios } from "../utils/customAxios";
+
+const successProps = {
+  open: true,
+  severity: "success",
+  message: "Successfuly Downloaded",
+};
+const errorProps = {
+  open: true,
+  severity: "error",
+  message: "There was a problem with your request",
+};
 
 export const About = () => {
+  const [setSnackbarProps, setIsLoading] = useOutletContext();
+
+  const handleDownload = async () => {
+    setIsLoading(true);
+    try {
+      let res = await ApiAxios.get("/cv", { responseType: "blob" }).catch(
+        (err) => {
+          console.error(err);
+          setSnackbarProps({ ...errorProps });
+        }
+      );
+      if (res && res.status === 200) {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement("a");
+        link.download = "Kate-Ramshaw-Curriculum-Vitae.pdf";
+        link.href = url;
+        link.target = "_self";
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        window.URL.revokeObjectURL(url);
+        setSnackbarProps({ ...successProps });
+      } else {
+        setSnackbarProps({ ...errorProps });
+      }
+    } catch (err) {
+      console.error(err);
+      setSnackbarProps({ ...errorProps });
+    }
+    setIsLoading(false);
+  };
+
   return (
     <Stack spacing={3}>
       <Header title="About" />
@@ -20,7 +66,7 @@ export const About = () => {
         eu rutrum sollicitudin. Curabitur eleifend est odio, eget ornare ex
         pellentesque eget.
       </Typography>
-      <Fab variant="extended" sx={{ width: 300 }}>
+      <Fab variant="extended" sx={{ width: 300 }} onClick={handleDownload}>
         Download CV <DownloadIcon sx={{ ml: 1 }} />
       </Fab>
     </Stack>
