b0e98481666d50057a01723ae39df89b0a814ef3
kate ramshaw <kate.ramshaw@talktalk.net>
2023-01-05 15:09:37 +0000

    updated cv controller

--- code/server/controllers/cv.js
+++ code/server/controllers/cv.js
@@ -1,4 +1,3 @@
-const config = require("../util/env-config");
 const getModulePath = require("../util/modulePath");
 const logger = require("../util/winston-config");
 
@@ -7,7 +6,7 @@ const logPath = { label: getModulePath(__filename) };
 const get = async (req, res, next) => {
   try {
     console.log("here it is");
-    res.status(200).download("./cvItGeneral.pdf");
+    res.status(200).download("./cvPortfolioFile.pdf");
   } catch (err) {
     logger.error(`Database report controller failed`, err, logPath);
     return next(extractError(err));
