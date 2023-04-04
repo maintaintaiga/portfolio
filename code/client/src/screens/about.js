79564a15622e9e962800f17972b01a1d5ebb5827
kate.ramshaw <kate.ramshaw@talktalk.net>
2023-04-04 20:03:32 +0100

    added about paragraph

--- code/client/src/screens/about.js
+++ code/client/src/screens/about.js
@@ -1,9 +1,10 @@
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, Link } from "react-router-dom";
 import { Button, Stack, Typography } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 
 import Header from "../components/header";
 import { ApiAxios } from "../utils/customAxios";
+import { about } from "../utils/cvInfo";
 
 const successProps = {
   open: true,
@@ -52,22 +53,15 @@ export const About = () => {
   return (
     <Stack spacing={3}>
       <Header title="About" />
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit
-        tellus vitae ipsum tristique, id commodo nibh scelerisque. Donec
-        pulvinar est tellus, vel efficitur massa euismod sit amet. Maecenas
-        facilisis vulputate volutpat. Integer porttitor consectetur tellus,
-        vitae rutrum ipsum. Ut nec ex aliquet nunc cursus pulvinar. Nulla
-        pretium, eros ac molestie scelerisque, lacus diam vestibulum orci, et
-        elementum mauris orci sed elit. Mauris cursus malesuada euismod. Nulla
-        sed interdum metus. Suspendisse rutrum sem eu viverra ultrices. Ut ac
-        malesuada massa. Cras nisi lorem, finibus et tempus a, malesuada sed
-        neque. Sed in viverra urna. Suspendisse potenti. Maecenas dignissim mi
-        eu rutrum sollicitudin. Curabitur eleifend est odio, eget ornare ex
-        pellentesque eget.
-      </Typography>
-      <Button variant="outlined" color="inherit" onClick={handleDownload}>
-        Download CV <DownloadIcon sx={{ ml: 1 }} />
+      <Typography>{about}</Typography>
+      <Button
+        variant="outlined"
+        color="inherit"
+        to="cv"
+        target="_blank"
+        LinkComponent={Link}
+      >
+        Cv
       </Button>
     </Stack>
   );
