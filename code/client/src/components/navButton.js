e2881c608c687755972269666c018d6e19b039e7
kate.ramshaw <kate.ramshaw@talktalk.net>
2023-04-04 14:43:01 +0100

    added navbutton component

--- code/client/src/components/navButton.js
+++ code/client/src/components/navButton.js
@@ -1,14 +1,20 @@
 import Button from "@mui/material/Button";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useMatch } from "react-router-dom";
 
 export default function NavButton({ label = "", url = "" }) {
+  let match = useMatch(url);
   return (
     <Button
       sx={{
         color: (theme) => (theme.palette.mode === "dark" ? "white" : "black"),
+        borderBottom: match ? 2 : 0,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
         "&:hover": {
           m: 1,
-          border: 1,
+          border: 2,
           fontSize: 15,
         },
       }}
