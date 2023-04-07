65bd4a69fe3db234ab1663a112bfa5b86862e7f3
kate.ramshaw <kate.ramshaw@talktalk.net>
2023-04-07 14:06:38 +0100

    fixed error in axios config withCredentials

--- code/client/src/utils/customAxios.js
+++ code/client/src/utils/customAxios.js
@@ -4,7 +4,7 @@ let baseUrl = process.env.REACT_APP_BASE_URL;
 
 const config = {
   baseURL: baseUrl,
-  useCredentials: true,
+  withCredentials: true,
 };
 
 const ApiAxios = axios.create(config);
@@ -12,11 +12,15 @@ const ApiAxios = axios.create(config);
 ApiAxios.interceptors.request.use(
   (config) => {
     let cookieArray = document.cookie.split(";");
+    console.log("document.cookie: ", document.cookie);
     let cookieIndex = cookieArray.findIndex(
       (i) => i.indexOf(process.env.REACT_APP_CSRF_TOKEN_COOKIE_NAME) !== -1
     );
+    console.log("cookie index: ", cookieIndex);
     let myCookie = cookieIndex >= 0 ? cookieArray[cookieIndex] : null;
+    console.log("my cookie/method: ", myCookie, config.method);
     if (myCookie && config.method !== "get") {
+      console.log("setting header");
       config.headers["csrf-token"] = myCookie.split("=")[1];
     }
     return config;
