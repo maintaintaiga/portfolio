7f5b2476c1efd1933e5815327e5be94a0ada3fa9
kate.ramshaw <kate.ramshaw@talktalk.net>
2023-04-18 11:14:58 +0100

    removed console logs

--- code/client/src/utils/customAxios.js
+++ code/client/src/utils/customAxios.js
@@ -12,15 +12,11 @@ const ApiAxios = axios.create(config);
 ApiAxios.interceptors.request.use(
   (config) => {
     let cookieArray = document.cookie.split(";");
-    console.log("document.cookie: ", document.cookie);
     let cookieIndex = cookieArray.findIndex(
       (i) => i.indexOf(process.env.REACT_APP_CSRF_TOKEN_COOKIE_NAME) !== -1
     );
-    console.log("cookie index: ", cookieIndex);
     let myCookie = cookieIndex >= 0 ? cookieArray[cookieIndex] : null;
-    console.log("my cookie/method: ", myCookie, config.method);
     if (myCookie && config.method !== "get") {
-      console.log("setting header");
       config.headers["csrf-token"] = myCookie.split("=")[1];
     }
     return config;
