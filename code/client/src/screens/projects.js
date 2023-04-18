f03af72270f80faa0c78cbfa048c2217805622b1
kate.ramshaw <kate.ramshaw@talktalk.net>
2023-04-18 11:17:19 +0100

    updated look of projects screen

--- code/client/src/screens/projects.js
+++ code/client/src/screens/projects.js
@@ -1,18 +1,11 @@
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid,
-  IconButton,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Box, Grid, IconButton, Stack, Typography } from "@mui/material";
 import LaunchIcon from "@mui/icons-material/Launch";
 
 import Tooltip from "../components/tooltip";
 import Header from "../components/header";
+import { Carousel } from "../components/carousel";
+
+const includeLink = false;
 
 const exampleProjects = [
   {
@@ -25,56 +18,49 @@ const exampleProjects = [
       Data storage was provided by the ArangoDB NOSQL database.
       The web server was provided by NginX which was also used as a reverse proxy for the server applications.
       Visual Studio Code was used as the development environment with a local server stack provided by WinNMP.`,
-    img: "/example-project-screenshot.png",
+    img: ["/screenshots/example-project-screenshot.png"],
   },
   {
     name: "Food Explorer",
     link: "https://food-explorer.com",
-    description: `Food explorer is a mobile first application utilizing react native,expo,react and
-      node.js throughout. A sqlite database is used to store data. The application 
+    description: `Food explorer is a mobile first application utilizing React Native, Expo, React and
+      Node.js throughout. A SQLite database is used to store data. The application 
       is deployed onto a public cloud provider. The app is used to search nutrition
       data and make it possible for users to track their nutrients.`,
-    img: "",
+    img: ["/screenshots/coming-soon.png"],
   },
 ];
 
 export const Projects = () => {
   return (
-    <Stack spacing={3}>
+    <Stack spacing={4}>
       <Header title="Projects" />
       <Box>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} justifyContent="flex-start">
           {exampleProjects.map((el) => (
-            <Grid key={el.name} item xs={12}>
-              <Card
-                elevation={0}
-                raised
-                sx={{ border: "2px solid", display: "flex" }}
-              >
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <CardContent sx={{ p: 1 }}>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {el.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {el.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions sx={{ py: 0 }}>
+            <Grid key={el.name} item xs={12} md={6}>
+              <Box sx={{ border: 2 }}>
+                {el.img && el.img.length > 0 ? (
+                  <Carousel images={el.img} />
+                ) : null}
+                <Box sx={{ p: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {el.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {el.description}
+                  </Typography>
+                </Box>
+                {includeLink ? (
+                  <Box sx={{ py: 0 }}>
                     <Tooltip title={`Go to ${el.name}`}>
                       <IconButton edge="start" href={el.link} target="_blank">
                         <LaunchIcon />
                       </IconButton>
                     </Tooltip>
-                  </CardActions>
-                </Box>
-                <CardMedia
-                  component="img"
-                  image={el.img}
-                  alt="Coming Soon"
-                  sx={{ height: 140 }}
-                />
-              </Card>
+                  </Box>
+                ) : null}
+              </Box>
             </Grid>
           ))}
         </Grid>
