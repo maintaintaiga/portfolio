f3ad4b20956754899f96516fe1dfc59d2d338f84
Kate Ramshaw <kate.ramshaw@talktalk.net>
2026-06-17 21:31:21 +0100

    feat(client): migrate App to react-router v7 RouterProvider/createBrowserRouter

--- code/client/src/App.tsx
+++ code/client/src/App.tsx
@@ -1,12 +1,32 @@
 import { useEffect, type JSX } from "react";
 import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
 import { lightTheme, darkTheme } from "./theme";
 import { ThemeContext } from "./context/theme";
 import { useDarkMode } from "./utils/useDarkMode";
-import { AppRoutes } from "./routes/app.routes";
+import { Home } from "./screens/home";
+import { About } from "./screens/about";
+import { Projects } from "./screens/projects";
+import { Contact } from "./screens/contact";
+import { Navigation } from "./components/navigation";
+import { CVDocument } from "./components/cv";
 import { CssBaseline } from "@mui/material";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigation />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "projects", element: <Projects /> },
+      { path: "contact", element: <Contact /> },
+      { path: "cv", element: <CVDocument /> },
+    ],
+  },
+]);
 
 function App(): JSX.Element {
   const [theme, toggleTheme, componentMounted] = useDarkMode();
@@ -23,9 +43,7 @@ function App(): JSX.Element {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <ThemeContext.Provider value={{ theme, toggleTheme: toggleTheme }}>
         <CssBaseline />
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </ThemeContext.Provider>
     </ThemeProvider>
   );
